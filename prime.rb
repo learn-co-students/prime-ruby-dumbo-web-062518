ccff71e80263c12ecb01f2c07f9a8497689e5eee
Daniel Chung <danetix@gmail.com>
2018-06-17 00:11:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+  
+  for i in 2..num-1 do
+    if num % i == 0
+      return false
+    end
+  end
+  
+  return true
+end

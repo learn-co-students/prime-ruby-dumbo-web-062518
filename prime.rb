89fc6809fcd91dd6477a4afb765c214f57f8302d
Jane Costa <yberdysh@gmail.com>
2018-06-21 10:13:49 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+	if num == 0 || num == 1 || num == -1
+		return false
+	elsif num > 0
+		for i in 2...num
+			return false if num/i.to_f % 1 == 0 
+		end 
+  		return true
+	else
+		for i in (num + 1)..-2
+			return false if num/i.to_f % 1 == 0 
+		end
+		return true
+	end 
+end
